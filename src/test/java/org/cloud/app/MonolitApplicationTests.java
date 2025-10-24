fe1be0d170d014baacae1da992a2f8289bdb9220
Makhonin <maxon.07@mail.ru>
2025-10-24 18:30:05 +0300

    init 2

---
+++ src/test/java/org/cloud/app/MonolitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cloud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
