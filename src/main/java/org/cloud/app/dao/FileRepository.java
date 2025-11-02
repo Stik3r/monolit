ca933191aaafc3d1ea6f2db5f6f4ea63d2ec1157
stik3r <maxon.07@mail.ru>
2025-11-02 03:04:37 +0300

    Начало работы

--- src/main/java/org/cloud/app/dao/FileRepository.java
+++ src/main/java/org/cloud/app/dao/FileRepository.java
@@ -4,8 +4,12 @@ import org.cloud.app.models.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
 
     File findByFileid(Long id);
+
+    List<File> findByParentId(Long parentId);
 }
